1c94bfb6454ccba19ea1ef57e865fd56402313cf
Jamie Johnson <jamiej@atomicmouse.co>
2018-12-22 00:09:07 -0600

    coding progress

--- wp-content/themes/understrap-child/panel-content/panel-scv.php
+++ wp-content/themes/understrap-child/panel-content/panel-scv.php
@@ -11,155 +11,50 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 
-<section class="container-fluid" id="dobox">
+<section class="container-fluid" id="scvbox">
 	<!-- 	<div class="row"> -->
 		<!-- <div class="container-fluid" id="hhelp"> -->
 
-			<div class="container" id="docontainer">
-				<div class="row">
+<?php get_template_part( 'navcluster-left' ); ?>
+
+
+
+			<div class="container" id="scvcontainer">
+		
+							
+						
+						
+				<div class="row" id="scvcontent">
 					<div class="pagetitle">
-						<hr>
-						<span>What I do</span>
+						<span>how i can help | 02 TRANSCEND THE COMPETITION</span>
 					</div>
 
-					
-						<div id="doheadline"><h4>I marry business goals with strategic frameworks and the best practices of modern and traditional marketing to bring about successful outcomes.</h4></div>
-						
-						<div class="row" id="docontent">
-						
 
-							<div class="do card">
-								<h5 class="card-title">Strategic Management</h5>
-
-								<hr>
-								<span>I approach marketing as a business discipline who's principle role is value creation and management for the customer, the company and collaborators.*</span>
-
-
-								<a class="btn btn-do" data-toggle="collapse" href="#do-card-1" role="button" aria-expanded="false" aria-controls="do-card-1">
-								<div class="btn-content d-flex mxauto justify-content-center align-items-center">+</div>
-								</a>
-								<div class="card-content collapse" id="do-card-1">
-									<hr><h6 class="card-subhead">Activities</h6>
-									Facilitating the design, promotion, exhange and delivery of customer value:
-
-									<hr>
-
-									<h6 class="card-subhead">Outcomes</h6>
-
-									<ul class="card-list">
-										<li>lorum ispum</li>
-										<li>sin dolor</li>
-										<li>ni amet</li>
-										<li>consetetur</li>
-										<li>sadipscing elit</li>
-										<li>sed diam</li>
-									</ul>
-
-								</div>
-							</div>
-
-<div class="do card">
-								<h5 class="card-title">Customer Engagement</h5>
-
-								<hr>
-								Attracting, earning and rewarding the trust of customers before, during and after the sale:
-
-
-								<a class="btn btn-do" data-toggle="collapse" href="#do-card-2" role="button" aria-expanded="true" aria-controls="do-card-1">
-								<div class="btn-content d-flex mxauto justify-content-center align-items-center">+</div>
-								</a>
-								<div class="card-content collapse" id="do-card-2">
-									<hr><h6 class="card-subhead">Activities</h6>
-									Facilitating the design, promotion, exhange and delivery of customer value:
-
-									<hr>
-
-									<h6 class="card-subhead">Outcomes</h6>
-
-									<ul class="card-list">
-										<li>lorum ispum</li>
-										<li>sin dolor</li>
-										<li>ni amet</li>
-										<li>consetetur</li>
-										<li>sadipscing elit</li>
-										<li>sed diam</li>
-									</ul>
-
-								</div>
-							</div>
-							<div class="do card">
-								<h5 class="card-title">Operations & infrastructure</h5>
-
-								<hr>
-								There's never been more potential for businesses to benefit from marketing. But doing so requires discipline, perspective and the acumen to pull it all together.
-
-
-								<a class="btn btn-do" data-toggle="collapse" href="#do-card-3" role="button" aria-expanded="true" aria-controls="do-card-1">
-								<div class="btn-content d-flex mxauto justify-content-center align-items-center">+</div>
-								</a>
-								<div class="card-content collapse" id="do-card-3">
-									<hr><h6 class="card-subhead">Activities</h6>
-									Aligning resources, capabilities and practices to effect productive marketing intiatives and maximize returns:
-
-									<hr>
-
-									<h6 class="card-subhead">Outcomes</h6>
-
-									<ul class="card-list">
-										<li>lorum ispum</li>
-										<li>sin dolor</li>
-										<li>ni amet</li>
-										<li>consetetur</li>
-										<li>sadipscing elit</li>
-										<li>sed diam</li>
-									</ul>
-
-								</div>
-							</div>
-							<div class="do card">
-								<h5 class="card-title">Content Development</h5>
-
-								<hr>
-								Today substance and authenticity are what convert prospects. Creativity and technology have their place, but neither can be effective without clear strategy guiding the way. 
-
-
-								<a class="btn btn-do" data-toggle="collapse" href="#do-card-4" role="button" aria-expanded="true" aria-controls="do-card-1">
-								<div class="btn-content d-flex mxauto justify-content-center align-items-center">+</div>
-								</a>
-								<div class="card-content collapse" id="do-card-4">
-									<hr><h6 class="card-subhead">Activities</h6>
-									Creating elements that reward and assist customers while reinforcing branding and positioning strategies:
-
-									<hr>
-
-									<h6 class="card-subhead">Outcomes</h6>
-
-									<ul class="card-list">
-										<li>lorum ispum</li>
-										<li>sin dolor</li>
-										<li>ni amet</li>
-										<li>consetetur</li>
-										<li>sadipscing elit</li>
-										<li>sed diam</li>
-									</ul>
-
-								</div>
-							</div>
-
-						
-
+					<div id="scvheadline">
+						<h2>Superior Customer Value</h2><span class="display-4">Competing in the customer-led economy</span>
+					</div>
+				<div class="row" id="scvblockrow">
+					<div class="scvblock">
+	<div class="scvshell"><div class="scvcopy">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</div></div>
+	<div class="scvshell"><div class="scvcopy">tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut iquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</div></div>
+	<div class="scvshell"><div class="scvcopy">
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></div>
+					</div>
+					<div class="scvblock">	<div class="scvshell"><div class="scvcopy">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</div></div>
+	<div class="scvshell"><div class="scvcopy">tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut iquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</div></div>
+	<div class="scvshell"><div class="scvcopy">
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></div>
 					
 					</div>
 				</div>
+</div>
+
 			</div>
 	
 
-<nav class="sidenav">
-				<div class="navcontent">
-				<div class=chevup><img src="http://jamiejohnsonmev2.test/wp-content/uploads/2018/12/chevup.svg" alt="" class="style-svg wp-image-23"/></div>
-				<div class=chevdn><img src="http://jamiejohnsonmev2.test/wp-content/uploads/2018/12/chevdn.svg" alt="" class="style-svg wp-image-24"/></div>
-			</div>
-			</nav>
+
 			
 		</section><!-- Container end -->
 
--- wp-content/themes/understrap-child/panel-content/panel-smm.php
+++ wp-content/themes/understrap-child/panel-content/panel-smm.php
@@ -21,21 +21,32 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 			<div class="container" id="smmcontainer">
 		
-				<div class="pagetitle">
+							
+						
+						
+				<div class="row" id="smmcontent">
+					<div class="pagetitle">
 						<span>how i can help | 01 ADVANCE BUSINESS OBJECTIVES</span>
 					</div>
 
-					
 
-					
-						<div id="smmheadline"><h2>Strategic Marketing Management</h2><span class="display-4">End to end expertise focused on business outcomes</span></div>
-						
-						<div class="row" id="smmcontent">
-							
-
-
-				
+					<div id="smmheadline">
+						<h2>Strategic Marketing Management</h2><span class="display-4">End to end leadership focused on business outcomes</span>
+					</div>
+					<img src="http://jamiejohnsonmev2.test/wp-content/uploads/2018/12/endtoend.svg" alt="" class="style-svg endtoend"/>
+					<div class="smmblock">
+						<div class="row">
+<div class="smmcopy"><span class="clead">Tie it all together.</span>
+Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris. </div>
+<div class="smmcopy"><span class="clead">Comprehensive leadership.</span> In voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident.</div>
+						</div>
+					</div>
 				</div>
+
+
 			</div>
 	
 

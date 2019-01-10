81f3804d56e97212ddc8fd13548c82fa6ebcae18
Jamie Johnson <jamiej@atomicmouse.co>
2019-01-10 13:06:04 -0600

    overdue update

--- wp-content/themes/understrap-child/panel-content/panel-howihelp.php
+++ wp-content/themes/understrap-child/panel-content/panel-howihelp.php
@@ -8,7 +8,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 ?>
-<section class="container-fluid" id="help-box">
+<section class="container-fluid register" id="help-box">
 	<div class="container" id="help-container">
 		<div class="row">
 			<div class="page-title">
@@ -16,12 +16,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 				<span>how i can help</span>
 			</div>
 			<div class=content id="help-content">
-				<div class="help headline"><h2>I create assets. Not overhead.</h2></div>
-				<div class="help subhead">After 20 years I've learned there are two types of marketers &#8212 spenders and contributors,</div>
+				<div class="help-headline"><h2>I create assets. Not overhead.</h2></div>
+				<div class="help-subhead">After 20 years I've learned there are two types people in this line of work: spenders &amp; marketers.</div>
 				
 				<div class="cblock" id="help-content-block">
 					
-					<div class="copy">Spenders create stuff and call it marketing. Contributors create assets that advance business.<span> I like to think of myself as a contributor.</span></div>
+					<div class="copy">Spenders create stuff and call it marketing. Marketers create assets that bring value to the business.<span> I like to think of myself as a marketer.</span></div>
 				</div>
 				
 				
@@ -30,12 +30,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<!-- 	<div class="row" id="help-content-row"> -->
 				<div class="help-links">
 					
-					<div class="copy" id="help-proclamations">I do more than execute marketing communications. I help businesses:</div>
+					<div class="copy" id="help-proclamations">How I Create Value for Businesses</div>
 					<ul>
-						<li><span>01</span><span class="help-links-li-content">advance business objectives</span></li>
-						<li><span>02</span><span class="help-links-li-content">transcend competition</span></li>
-						<li><span>03</span><span class="help-links-li-content">modernize revenue generation</span></li>
-						<li><span>04</span><span class="help-links-li-content">optimize marketing investments</span></li>
+						<li><span>01</span><span class="help-links-li-content">Advancing Business Objectives</span></li>
+						<li><span>02</span><span class="help-links-li-content">Transcending Competition</span></li>
+						<li><span>03</span><span class="help-links-li-content">Modernizing The Marketing Function</span></li>
+						<li><span>04</span><span class="help-links-li-content">Optimizing Marketing Investments</span></li>
 					</ul>
 				</div>
 		<!-- 	</div> -->
--- wp-content/themes/understrap-child/panel-content/panel-ido.php
+++ wp-content/themes/understrap-child/panel-content/panel-ido.php
@@ -8,7 +8,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 exit; // Exit if accessed directly.
 }
 ?>
-<section class="container-fluid" id="dobox">
+<section class="container-fluid" id="do-box">
 	
 	<div class="container" id="do-container">
 		<div class="row">
@@ -16,27 +16,32 @@ exit; // Exit if accessed directly.
 				<hr>
 				<span>What I do</span>
 			</div>
-			<div class="row" id="do-content">
-				<div class="do headline"><h2>I Provide End to End Marketing Leadership</h2></div><div class="do subhead">Strategy, Operations, Development & Execution</div>
+	<div class="row" id="do-content">
+	<div class="do-headline">I expect marketing to perform like any other business function.</div>
+				<div class="do-subhead">I combine 2o years of strategic marketing expertise with proficiencies in web development and design to achieve business outcomes. </div>
+				<!-- <div class="do subhead">Strategy, Operations, Development & Execution</div>
 				<div class="row" id="do-content-row">
 					<div id="do-content-block"><div class="copy">I marry business objectives with strategic marketing frameworks and the best practices of modern and traditional marketing.</div></div>
-				</div>
-				<div class="card-head copy">Laboris elit excepteur nisi.</div>
-					<hr>
+				</div> -->
+				<!-- <div class="card-head copy">Laboris elit excepteur nisi.</div>
+					 -->
 				<div class="row container-cards">
 					
 					<div class="do card">
-						<div class="card-intro"><h6 class="card-title">Value Creation</h6>
-						<hr>
-						<span><span class="lead">For me marketing is a business discipline.</span> It's primary objective is to help advance the business. It's mission is to create customers. Promotion is a means, not the end.</span></div>
+						<div class="div-width" id="do-card-1-width">
+						<div class="card-intro"><h4 class="card-title">Strategic Marketing Management</h4>
+						
+						<div class="card-summary"><span><span class="lead">Everyting I do begins and ends with business goals.</span> Veniam anim ad in amet et veniam sed culpa.</span></div></div>
 						<a class="do-toggle-1 collapsed" data-toggle="collapse" href="#do-card-1" aria-expanded="true" aria-controls="do-card-1"><span>more</span><b>less</b></a>
 				
 						<div class="card-content collapse" id="do-card-1">
-							<div class="card-body"><hr><h6 class="card-subhead">Activities</h6>
+							
+							<div class="card-body"><h4 class="card-subhead">Activities</h4>
 							<span>Facilitating the design, promotion, exhange and delivery of customer value.</span></div>
-							<hr>
-							<h6 class="card-subhead">Outcomes</h6>
+							
+						
 							<ul class="card-list">
+									<h4 class="card-subhead">Outcomes</h4>
 								<li>lorum ispum</li>
 								<li>sin dolor</li>
 								<li>ni amet</li>
@@ -46,16 +51,18 @@ exit; // Exit if accessed directly.
 							</ul>
 						</div>
 					</div>
+					</div>
 					<div class="do card">
-						<div class="card-intro"><h6 class="card-title">Customer Engagement</h6>
-						<hr>
-						<span><span class="lead">Engagement is the primary activity of modern marketing &#8212</span>ensuring consistent and quality customer experiences from beginning to end of the cycle.</span></div>
+						<div class="div-width" id="do-card-2-width">
+						<div class="card-intro"><h4 class="card-title">Customer Engagement</h4>
+						
+						<span><span class="lead">Engagement is the primary activity of modern marketing &#8212</span> ensuring consistent and quality customer experiences from beginning to end of the cycle.</span></div>
 						<!-- 	<a class="btn btn-do" data-toggle="collapse" href="#do-card-2" role="button" aria-expanded="true" aria-controls="do-card-1"> -->
 						<a class="do-toggle-2 collapsed" data-toggle="collapse" href="#do-card-2" aria-expanded="true" aria-controls="do-card-2"><span>more</span><b>less</b></a>
 						<div class="card-content collapse" id="do-card-2">
-							<div class="card-body"><hr><h6 class="card-subhead">Activities</h6>
+							<div class="card-body"><h6 class="card-subhead">Activities</h6>
 							<span>Attracting, earning and rewarding the trust of customers before, during and after the sale.</span></div>
-							<hr>
+							
 							<h6 class="card-subhead">Outcomes</h6>
 							<ul class="card-list">
 								<li>lorum ispum</li>
@@ -67,17 +74,19 @@ exit; // Exit if accessed directly.
 							</ul>
 						</div>
 					</div>
+					</div>
 					<div class="do card">
+						<div class="div-width" id="do-card-3-width">
 						<div class="card-intro"><h6 class="card-title">Operations & infrastructure</h6>
-						<hr>
+						
 						<span><span class="lead">There's never been more potential for businesses to benefit from marketing.</span> But doing so requires discipline, perspective and the acumen to pull it all together.
 						</span>
 					</div>
 						<a class="do-toggle-3 collapsed" data-toggle="collapse" href="#do-card-3" aria-expanded="true" aria-controls="do-card-3"><span>more</span><b>less</b></a>
 						<div class="card-content collapse" id="do-card-3">
-							<div class="card-body"><hr><h6 class="card-subhead">Activities</h6>
+							<div class="card-body"><h6 class="card-subhead">Activities</h6>
 							<span>Aligning resources, capabilities and practices to effect productive marketing intiatives and maximize returns:</span></div>
-							<hr>
+							
 							<h6 class="card-subhead">Outcomes</h6>
 							<ul class="card-list">
 								<li>lorum ispum</li>
@@ -89,15 +98,16 @@ exit; // Exit if accessed directly.
 							</ul>
 						</div>
 					</div>
-					<div class="do card">
+					</div>
+<!-- 					<div class="do card">
 						<div class="card-intro"><h6 class="card-title">Content Development</h6>
-						<hr>
+						
 						<span><span class="lead">Today substance and authenticity drive prospect conversion.</span> Creativity and technology are imperatives and neither is effective without guidance.</span></div>
 						<a class="do-toggle-4 collapsed" data-toggle="collapse" href="#do-card-4" aria-expanded="true" aria-controls="do-card-4"><span>more</span><b>less</b></a>
 						<div class="card-content collapse" id="do-card-4">
-							<div class="card-body"><hr><h6 class="card-subhead">Activities</h6>
+							<div class="card-body"><h6 class="card-subhead">Activities</h6>
 							<span>Creating elements that reward and assist customers while reinforcing branding and positioning strategies:</span></div>
-							<hr>
+							
 							<h6 class="card-subhead">Outcomes</h6>
 							<ul class="card-list">
 								<li>lorum ispum</li>
@@ -108,7 +118,7 @@ exit; // Exit if accessed directly.
 								<li>sed diam</li>
 							</ul>
 						</div>
-					</div>
+					</div> -->
 				</div>
 			</div>
 		</div>

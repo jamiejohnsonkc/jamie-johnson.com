6ea85e453d53e4c4cc05370dbea6e44537ecd097
Jamie Johnson <jamie@jamie-johnson.com>
2019-11-18 09:51:45 -0600

    remove typekit from functions, updated header
    Signed-off-by: Jamie Johnson <jamie@jamie-johnson.com>

--- wp-content/themes/understrap-child/functions.php
+++ wp-content/themes/understrap-child/functions.php
@@ -1,47 +1,56 @@
 <?php
-function understrap_remove_scripts() {
-    wp_dequeue_style( 'understrap-styles' );
-    wp_deregister_style( 'understrap-styles' );
 
-    wp_dequeue_script( 'understrap-scripts' );
-    wp_deregister_script( 'understrap-scripts' );
+function understrap_remove_scripts()
+{
+    wp_dequeue_style('understrap-styles');
+    wp_deregister_style('understrap-styles');
+
+    wp_dequeue_script('understrap-scripts');
+    wp_deregister_script('understrap-scripts');
 
     // Removes the parent themes stylesheet and scripts from inc/enqueue.php
 }
-add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
+add_action('wp_enqueue_scripts', 'understrap_remove_scripts', 20);
 
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
-
-	// Get the theme data
-	$the_theme = wp_get_theme();
-    wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
-    wp_enqueue_script( 'jquery');
-    wp_enqueue_script( 'popper-scripts', get_stylesheet_directory_uri() . '/js/popper.min.js', array(), false);
-    wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
+    // Get the theme data
+    $the_theme = wp_get_theme();
+    wp_enqueue_style('child-understrap-styles', get_stylesheet_directory_uri().'/css/child-theme.min.css', array(), $the_theme->get('Version'));
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('popper-scripts', get_stylesheet_directory_uri().'/js/popper.min.js', array(), false);
+    wp_enqueue_script('child-understrap-scripts', get_stylesheet_directory_uri().'/js/child-theme.min.js', array(), $the_theme->get('Version'), true);
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
     }
 }
 
-function add_child_theme_textdomain() {
-    load_child_theme_textdomain( 'understrap-child', get_stylesheet_directory() . '/languages' );
+function add_child_theme_textdomain()
+{
+    load_child_theme_textdomain('understrap-child', get_stylesheet_directory().'/languages');
 }
-add_action( 'after_setup_theme', 'add_child_theme_textdomain' );
-
-
+add_action('after_setup_theme', 'add_child_theme_textdomain');
 
 //! my custom functions
 
-function understrap_change_logo_class( $html ) {
-        $html = str_replace( 'class="custom-logo"', 'class="style-svg img-fluid"', $html );
-        return $html;
-    }
-
-function contact_page_recaptcha() {
-if ( is_page( '12' ) ) {
-wp_enqueue_script('google-recaptcha', 'https://www.google.com/recaptcha/api.js');
+function enqueue_adobe_fonts_stylesheet()
+{
+    wp_enqueue_style('adobe_fonts_stylesheet', 'https://use.typekit.net/wwp3qhw.css', 'all');
 }
+    add_action('wp_enqueue_scripts', 'enqueue_adobe_fonts_stylesheet');
+
+function understrap_change_logo_class($html)
+{
+    $html = str_replace('class="custom-logo"', 'class="style-svg img-fluid"', $html);
+
+    return $html;
+}
+
+function contact_page_recaptcha()
+{
+    if (is_page('12')) {
+        wp_enqueue_script('google-recaptcha', 'https://www.google.com/recaptcha/api.js');
+    }
 }
 add_action('wp_enqueue_scripts', 'contact_page_recaptcha');
-    
--- wp-content/themes/understrap-child/header.php
+++ wp-content/themes/understrap-child/header.php
@@ -32,7 +32,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="stylesheet" href="https://use.typekit.net/hny8rtu.css">
 	<?php wp_head(); ?>
 </head>
 
